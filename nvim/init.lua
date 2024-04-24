d5617cd5c9446feb665dd0135f38fcc78d54413b
Ian Teves <iantevesacc@gmail.com>
2024-04-24 12:03:41 +1200

    Initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,65 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+-- Enable system clipboard support
+vim.opt.clipboard:append("unnamedplus")
+
+-- Normal mode mapping
+vim.api.nvim_set_keymap('n', 'y', '"+y', { noremap = true })
+
+-- Visual mode mapping
+vim.api.nvim_set_keymap('v', 'y', '"+y', { noremap = true })
+
+-- Set the clipboard configuration
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
+
+-- Set relativenumber option to true
+vim.opt.relativenumber = true
+
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
